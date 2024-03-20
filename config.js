a3237d5ad86f1a1f9d64e8c817537e5ded2e1ef9
xsspidey <164045721+xsspidey@users.noreply.github.com>
2024-03-20 08:29:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,11 +22,11 @@ global.APIs = {
 
 // Other
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = '𝑆𝛪𝐷 𝐵𝛨𝛥𝛪';
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
-global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢';
-global.author = '𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫';
+global.ownername = '𝐒𝐏𝐘𝐃𝐄𝐑';
+global.owner = ['919091305289'];
+global.premium = ['919091305289'];
+global.packname = '𝐒𝚸𝐘𝐃𝐱𝐑🍁';
+global.author = '𝐒𝐏𝐘𝐃𝐄𝐑';
 global.sessionName = 'session';
 global.prefa = ['', '!', '.'];
 global.sp = '✪';
@@ -43,8 +43,8 @@ global.mess = {
     wait: '*Processing Your request*',
     endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
 };
-global.link = 'https://whatsapp.com/channel/0029VaFNrVHBfxoBuY2TzH2w'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.link = 'https://www.instagram.com/sxy.spy/'
+global.linkGroup = '𝐒𝐏𝐘𝐃𝐄𝐑'
 global.autoswview = process.env.AUTO_STATUS_SEEN || "true",
 global.typemenu = 'v1' // 'v1' => 'v5'
 global.onlygroup = false // onlygroup
